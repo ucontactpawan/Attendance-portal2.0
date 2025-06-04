8b11e0f1ea115ac1ce4113c00af5056ba4a3b167
pawan <ucontactpawan@gmail.com>
2025-06-05 00:59:07 +0530

    second-commit

--- login.php
+++ login.php
@@ -1,38 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"/>
-
+    <title>login page</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
 </head>
+
 <body>
     <div class="container">
-        <div class="form-box login">
-            <form action="login.ph" method="post">
-                <h1>Login</h1>
-                <div class="input-box">
-                    <input type="text" name="username" required>
-                    <i class="fa-solid fa-user"></i>
-                </div>
-                <div class="input-box">
-                    <input type="password" name="password" required>
-                    <i class="fa-solid fa-lock"></i>
-                </div>
-                <div class="forgot-link">
-                    <a href="#">Forgot password?</a>
-                </div>
-                <button type="submit" class="btn">Login</button>
-                <p>or login with social media</p>
-                <div class="social-icons">
-                    
-                </div>
-            </form>
-
+        <h1 class="form-title">
+            Sign In
+        </h1>
+        <form method="POST" action="">
+            <div class="input-group">
+                <i class="fas fa-envelope"></i>
+                <input type="email" name="email" id="email" required placeholder="Email">
+            </div>
+            <div class="input-group">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="password" id="password" required placeholder="Password">
+                <i class="fa fa-eye" id="eye"></i>
+            </div>
+            <p class="recover">
+                <a href="#">Recover Password</a>
+            </p>
+            <input type="submit" class="btn" value="Sign In" name="signin">
+        </form>
+        <p class="or">
+            --------or--------
+        </p>
+        <div class="icons">
+            <i class="fab fa-google"></i>
+            <i class="fab fa-facebook"></i>
         </div>
-
+        <div class="links">
+            <p>Don't have account yet?</p>
+            <a href="register.php">Sign up</a>
+        </div>
     </div>
-    
+
+    <script src="js/script.js"></script>
 </body>
+
 </html>
