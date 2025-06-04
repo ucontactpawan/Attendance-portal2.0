8b669474264c7d34fd178441c5b55aa1964857ff
pawan <ucontactpawan@gmail.com>
2025-06-05 01:07:36 +0530

    third-commit

---
+++ register.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register page</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="form-title">Register</h1>
+        <form method="POST" action="">
+            <div class="input-group">
+                <i class="fas fa-user"></i>
+                <input type="text" name="name" id="name" placeholder="Name" required>
+            </div>
+            <div class="input-group">
+                <i class="fas fa-envelope"></i>
+                <input type="email" name="email" id="email" placeholder="Email" required>
+            </div>
+            <div class="input-group password">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="password" id="password" placeholder="Password" required>
+                <i id="eye" class="fa fa-eye"></i>
+            </div>
+
+            <div class="input-group">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="confirm_password" placeholder="confirm password">
+            </div>
+            <input type="submit" class="btn" value="Sign Up" name="signup">
+        </form>
+        <p class="or">
+            ----------or--------
+        </p>
+        <div class="icons">
+            <i class="fab fa-google"></i>
+            <i class="fab fa-facebook"></i>
+        </div>
+        <div class="links">
+            <p>Don't have account yet?</p>
+            <a href="register.php">Sign Up</a>
+        </div>
+    </div>
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
