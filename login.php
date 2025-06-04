34679e5e425893537f83d5b3b4df107f0b7097c5
pawan singh <ucontactpawan@gmail.com>
2025-06-04 18:27:28 +0530

    Initial commit

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"/>
+
+</head>
+<body>
+    <div class="container">
+        <div class="form-box login">
+            <form action="login.ph" method="post">
+                <h1>Login</h1>
+                <div class="input-box">
+                    <input type="text" name="username" required>
+                    <i class="fa-solid fa-user"></i>
+                </div>
+                <div class="input-box">
+                    <input type="password" name="password" required>
+                    <i class="fa-solid fa-lock"></i>
+                </div>
+                <div class="forgot-link">
+                    <a href="#">Forgot password?</a>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <p>or login with social media</p>
+                <div class="social-icons">
+                    
+                </div>
+            </form>
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
